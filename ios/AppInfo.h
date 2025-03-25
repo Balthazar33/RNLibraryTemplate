089d657a658680f18758bb5a06648172c03aab52
Balthazar33 <castielcollins57@yahoo.in>
2025-03-25 10:40:06 +0530

    ios native code

--- ios/AppInfo.h
+++ ios/AppInfo.h
@@ -1,6 +1,11 @@
 
-#import "generated/RNAppInfoSpec/RNAppInfoSpec.h"
+// #import "generated/RNAppInfoSpec/RNAppInfoSpec.h"
 
-@interface AppInfo : NSObject <NativeAppInfoSpec>
+// @interface AppInfo : NSObject <NativeAppInfoSpec>
 
+// @end
+// TODO:
+#import <React/RCTBridgeModule.h>
+
+@interface AppInfo : NSObject <RCTBridgeModule>
 @end
--- ios/AppInfo.mm
+++ ios/AppInfo.mm
@@ -1,18 +1,57 @@
+// #import "AppInfo.h"
+
+// @implementation AppInfo
+// RCT_EXPORT_MODULE()
+
+// - (NSNumber *)multiply:(double)a b:(double)b {
+//     NSNumber *result = @(a * b);
+
+//     return result;
+// }
+
+// - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+//     (const facebook::react::ObjCTurboModule::InitParams &)params
+// {
+//     return std::make_shared<facebook::react::NativeAppInfoSpecJSI>(params);
+// }
+
+// @end
+// TODO:
+
 #import "AppInfo.h"
+#import <React/RCTLog.h>
 
 @implementation AppInfo
-RCT_EXPORT_MODULE()
 
-- (NSNumber *)multiply:(double)a b:(double)b {
-    NSNumber *result = @(a * b);
+RCT_EXPORT_MODULE();
 
-    return result;
-}
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
+RCT_EXPORT_METHOD(getAppVersion:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    return std::make_shared<facebook::react::NativeAppInfoSpecJSI>(params);
+  @try {
+    NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+    NSString *versionName = infoDictionary[@"CFBundleShortVersionString"];
+    NSString *versionCode = infoDictionary[@"CFBundleVersion"];
+
+    if (versionName && versionCode) {
+      NSDictionary *versionInfo = @{
+        @"versionName": versionName,
+        @"versionCode": versionCode
+      };
+      resolve(versionInfo);
+    } else {
+      NSError *error = [NSError errorWithDomain:@"AppInfo"
+                                           code:1001
+                                       userInfo:@{NSLocalizedDescriptionKey:@"Version information not found"}];
+      reject(@"no_version_info", @"Version information not found", error);
+    }
+  }
+  @catch (NSException *exception) {
+    NSError *error = [NSError errorWithDomain:@"AppInfo"
+                                         code:1002
+                                     userInfo:@{NSLocalizedDescriptionKey:exception.reason}];
+    reject(@"exception", exception.reason, error);
+  }
 }
 
 @end
